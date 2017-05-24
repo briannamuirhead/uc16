d19ff20eca7854809cf51d0fb008979491cd0f02
SineadSwayne <ssinead3@gmail.com>
2017-05-24 08:22:48 -0700

    Updated

---
+++ uc16.js
@@ -0,0 +1,49 @@
+/**
+ * Uses AJAX to query an internet data source for zip codes
+ * @param {string} zipId The element id that has the zip code
+ */
+function findRecipe(recipeId) {
+    // First get the zip code from the HTML textbox
+    var recipe = document.getElementById(recipeId).value;
+    // Now make a HTTP request
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange = function () {
+        if (this.readyState === 4) {
+            // We got a response from the server!
+            if(this.status === 200) {
+                // The request was successful!
+                displayPlace(this.responseText);
+            } else if (this.status === 404){
+                // No postal code found
+                displayRecipe('{ "country" : "none" }');
+            } else {
+                console.log("We have a problem...server responded with code: " + this.status);
+            }
+        } else {
+            // Waiting for a response...
+        }
+    };
+    // Notice how the URL is appended with the zip code
+    var url = "http://api.zippopotam.us/us/" + recipe;
+    httpRequest.open("GET", url, true);
+    httpRequest.send();
+}
+
+/**
+ * Displays the zip code place given the JSON data
+ * @param {string} data JSON data representing place for given zip code
+ */
+function displayPlace(data){
+    var place = JSON.parse(data);
+    if(place.country === "none") {
+        document.getElementById("place").className = "alert alert-warning";
+        document.getElementById("place").innerHTML = "No place matches that zip code."
+    } else {
+        document.getElementById("place").className = "alert alert-success";
+        document.getElementById("place").innerHTML = place.places[0]["place name"] +
+        ", " +
+        place.places[0].state +
+        ", " +
+        place.country;
+    }
+}
