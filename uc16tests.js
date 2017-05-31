91de962bc28ba26fd6cc17783c9ae1203e34d09f
Brianna B Muirhead <bm66627@srcs.loc>
2017-05-31 09:08:05 -0700

    test

--- uc16tests.js
+++ uc16tests.js
@@ -1,15 +1,7 @@
-// Gets data when given input search parameters
-QUnit.test( "Gets data test", function( assert ) {
-    addDigit('1');
-    addDigit('2');
-    assert.equal(document.getElementById("screen").value, "12", "Passed - Expected 12");
-});
 
 // Displays an error message when the search criteria does not find anything in the data source.
 QUnit.test( "Displays an error message", function( assert ) {
-    clearCurrent();
-    addDecimal();
-    addDigit('2');
-    addDecimal();
-    assert.equal(document.getElementById("screen").value, "0.2", "Passed - Expected 0.2");
+
+    findWeather("gg");
+    assert.equal(document.getElementById("weather").value, "There is no city found with that name.", "Passed - Expected 0.2");
 });
